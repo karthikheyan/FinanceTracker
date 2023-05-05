4dc5ceb3f2316800093c9f4e4661b31597cf95cf
Karthi <karthivelu7229@gmail.com>
2023-05-05 19:27:55 +0530

    navbar and login details

--- my-money/src/pages/Login/Login.js
+++ my-money/src/pages/Login/Login.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Login.module.css'
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e)=> {
+        e.preventDefault()
+        console.log(email,password)
+    }
   return (
     <div>
-      Login
+      <form onSubmit={handleSubmit} className={styles['login-form']}>
+        <label>
+            <span>Email</span>
+            <input 
+            type="text"
+            onChange={(e)=> setEmail(e.target.value)}
+            value={email}
+             />
+        </label>
+
+        <label>
+            <span>Password</span>
+            <input 
+            type="password"
+            onChange={(e)=> setPassword(e.target.value)}
+            value={password}
+             />
+        </label>
+
+        <button className='btn'>Login</button>
+      </form>
     </div>
   )
 }
