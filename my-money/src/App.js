482a3edf95deb2a87e06b375cb49778badecb790
Karthi <karthivelu7229@gmail.com>
2023-05-05 19:05:47 +0530

    setting up pages and routes

--- my-money/src/App.js
+++ my-money/src/App.js
@@ -1,9 +1,23 @@
-import './App.css'
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+          <Route path='/login'>
+            <Login/>
+          </Route>
+          <Route path='/signup'>
+            <Signup/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ my-money/src/pages/Signup/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Signup.module.css'
+
+const Signup = () => {
+  return (
+    <div>
+      signup
+    </div>
+  )
+}
+
+export default Signup
